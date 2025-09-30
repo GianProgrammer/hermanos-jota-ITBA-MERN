f1b4ee5a66c833e6bd1258ff9870505543cd2bd7
GianProgrammer <gianferreyra2014@gmail.com>
2025-09-30 00:50:42 -0300

    css added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import NavBar from './components/navbar'
+import './styles/app.css'
 
 function App() {
   return (
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-
+import '../styles/navbar.css'
 
 function navBar() {
     return (
