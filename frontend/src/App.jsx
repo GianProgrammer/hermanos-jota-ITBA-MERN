082bd4bd6c40ced89934d766dc490345a3947652
GianProgrammer <gianferreyra2014@gmail.com>
2025-09-30 22:16:04 -0300

    creacion correcta de routes en app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 // import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
 import NavBar from './components/navbar'
 import './styles/app.css'
 import Footer from './components/Footer';
@@ -7,6 +8,11 @@ function App() {
   return (
     <>
       <NavBar/>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/productos" element={<h1>Productos</h1>} />
+        <Route path="/contacto" element={<h1>About</h1>} />
+      </Routes>
       <Footer/>
     </>    
   )
