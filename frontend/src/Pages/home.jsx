8a102adb54e0a54910fc4a1f7e9278d853fd7805
GianProgrammer <gianferreyra2014@gmail.com>
2025-11-02 13:44:29 -0300

    minor changes 2

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -1,6 +1,7 @@
 import "../styles/home.css";
 import productos from "../../../backend/productos";
 import ListaProductos from "../components/Listaproductos";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -8,7 +9,9 @@ function Home() {
       <section className="hero">
         <div className="hero-content">
           <h2>Diseño Artesanal,<br />Calidad Garantizada</h2>
-          <a href="/productos" className="btn">Ver Catálogo</a>
+          <Link to="/productos" className="btn">
+            Ver Catálogo
+          </Link>
         </div>
       </section>
 
