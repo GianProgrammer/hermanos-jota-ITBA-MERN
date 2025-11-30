114f6d25bed72324089078fde5d7ab397b7c6758
Franrobles123 <franrobles377@gmail.com>
2025-11-30 20:42:43 -0300

    Acomode la navbar

--- frontend/src/Pages/Registrarse.jsx
+++ frontend/src/Pages/Registrarse.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/register.css";
+import "../styles/Register.css";
 
 function Register() {
   const [formData, setFormData] = useState({
